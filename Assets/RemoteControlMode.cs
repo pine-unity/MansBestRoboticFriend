aa98f4da1f1b45b9230fd9f727704249de2c6c14
pine-unity <pineymations@gmail.com>
2026-04-23 11:06:18 -0500

    basic movement, will add rotation later and fix some collision stuff

--- Assets/RemoteControlMode.cs
+++ Assets/RemoteControlMode.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class RemoteControlMode : MonoBehaviour
 {
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 40;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move();
     }
+
+    // move forward/backward/left/right
+    void move(){
+        float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        float horizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+
+        gameObject.transform.position += new Vector3(horizontal, 0, vertical);
+    }
+
+    void rotate(){
+
+    }
+
 }
