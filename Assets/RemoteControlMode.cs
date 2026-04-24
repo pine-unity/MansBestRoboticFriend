59b01aa1ec913a727cff53aa68f3d4b4dcdd263f
pine-unity <pineymations@gmail.com>
2026-04-24 11:46:16 -0500

    i had to redo the movement to be less concise rip :(

--- Assets/RemoteControlMode.cs
+++ Assets/RemoteControlMode.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class RemoteControlMode : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float angle;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 40;
+        speed = 50;
+        angle = 65;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,14 +23,53 @@ public class RemoteControlMode : MonoBehaviour
 
     // move forward/backward/left/right
     void move(){
-        float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        float horizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+    
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = -1 * transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = -1 * transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * speed;
+        }    
 
-        gameObject.transform.position += new Vector3(horizontal, 0, vertical);
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = Vector3.zero;
+        }  
+
+        rotate();
+         
     }
 
     void rotate(){
-
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -1 * angle * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, angle * Time.deltaTime, 0);
+        }
     }
+
+
+    void onCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("block"))
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
 
 }
