da4cd4065cd96525f02196b9a46e336100304fa6
pine-unity <pineymations@gmail.com>
2026-05-03 18:04:40 -0500

    fixed compiler errors

--- Assets/PersonalityCalculation.cs
+++ Assets/PersonalityCalculation.cs
@@ -14,9 +14,18 @@ public class PersonalityCalculation : MonoBehaviour
     float total;
     float normVal;
 
-    // Start is called before the first frame update
+    string emotion;
+
+    float happyScale;
+    float sadScale;
+    float angerScale;
+
     void Start()
     {
+        happyScale = 1.0f;
+        sadScale = 1.0f;
+        angerScale = 1.0f;
+
         hyperactivity = 500;
         laziness = 500;
         total = hyperactivity + laziness;
@@ -25,13 +34,48 @@ public class PersonalityCalculation : MonoBehaviour
         totalScolds = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         difference = hyperactivity - laziness;
         normVal = difference / (total + 1);
     }
 
+    // will implement action-specific praises/scolds later
+    public void praiseIncrease()
+    {
+        totalPraises++;
+
+        // make him more happy :3
+        if(happyScale <= 0.5)
+        {
+            happyScale += 0.5f;
+        } else if(happyScale < 1)
+        {
+            float difference = 1 - happyScale;
+            happyScale += 1 - difference;
+        }
+
+        Debug.Log(totalPraises);
+    }
+
+    public void scoldIncrease()
+    {
+        totalScolds++;
+
+        // make him more ANGRY >:(
+        if(angerScale <= 0.5)
+        {
+            angerScale += 0.5f;
+        } else if(angerScale < 1)
+        {
+            float difference = 1 - angerScale;
+            angerScale += 1 - difference;
+        }
+
+        Debug.Log(totalScolds);
+    }
+
+    // bayesian probability
     float calculateProbability(float praises, float scolds)
     {
         float finalProbability;
